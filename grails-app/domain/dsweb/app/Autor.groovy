c97f18f648788178841f784a6a9bb2442a0fa8c7
Leandro Novak <leandro.novak@outlook.com>
2017-07-07 19:57:13 -0300

    updated some entities

--- grails-app/domain/dsweb/app/Autor.groovy
+++ grails-app/domain/dsweb/app/Autor.groovy
@@ -1,7 +1,15 @@
 package dsweb.app
 
 class Autor {
+    String nome
+    String pseudonimo
+    Date dataNascimento
+    String biografia
 
     static constraints = {
+        nome blank: true, size: 1..255
+        pseudonimo blank: true, size: 1..255
+        dataNascimento nullable: false
+        biografia size: 1..1023
     }
 }
--- grails-app/domain/dsweb/app/Diretor.groovy
+++ grails-app/domain/dsweb/app/Diretor.groovy
@@ -1,7 +1,13 @@
 package dsweb.app
 
 class Diretor {
+    String nome
+    Date dataNascimento
+    String biografia
 
     static constraints = {
+        nome blank: true, size: 1..255
+        dataNascimento nullable: false
+        biografia size: 1..1023
     }
 }

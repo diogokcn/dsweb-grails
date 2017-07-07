fe7191ad5e8d5ec070d42f4832ada01534b9dc32
Leandro Novak <leandro.novak@outlook.com>
2017-07-07 19:21:40 -0300

    added usuario and ator

--- grails-app/domain/dsweb/app/Ator.groovy
+++ grails-app/domain/dsweb/app/Ator.groovy
@@ -1,13 +1,17 @@
 package dsweb.app
 
 class Ator {
-    String nome
+    String nomeReal
+    String nomeArtistico
     Date dataNascimento
-    String login
-    String password
-    String email
-    Boolean isAdmin
-    
+    String localNascimento
+    String biografia
+
     static constraints = {
+        nomeReal blank: true, size: 1..255
+        nomeArtistico blank: false, size: 1..255
+        dataNascimento nullable: false
+        localNascimento size: 1..255
+        biografia size: 1..1023
     }
 }
--- grails-app/domain/dsweb/app/Usuario.groovy
+++ grails-app/domain/dsweb/app/Usuario.groovy
@@ -1,7 +1,18 @@
 package dsweb.app
 
 class Usuario {
+    String nome
+    Date dataNascimento
+    String login
+    String password
+    String email
+    Boolean isAdmin
 
     static constraints = {
+        nome blank: false, size: 1..255
+        dataNascimento nullable: false
+        login blank: false, size: 1..255
+        password blank: false, size: 4..255, password: true
+        email blank: false, size: 1..255, email: true
     }
 }

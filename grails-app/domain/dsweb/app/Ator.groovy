373ff4105477a27385cf3e48f73a444599456d82
Leandro Novak <leandro.novak@outlook.com>
2017-07-07 18:53:16 -0300

    updated classes

--- grails-app/domain/dsweb/app/Ator.groovy
+++ grails-app/domain/dsweb/app/Ator.groovy
@@ -1,7 +1,13 @@
 package dsweb.app
 
 class Ator {
-
+    String nome
+    Date dataNascimento
+    String login
+    String password
+    String email
+    Boolean isAdmin
+    
     static constraints = {
     }
 }
